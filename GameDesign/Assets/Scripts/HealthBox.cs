5d638383fec0574798a22124e159a515cb163bf8
BaciuMaria <laurabaciu31@yahoo.com>
2025-03-14 18:10:31 +0200

    fixed boxes, hurt and dying anim + player can respawn after death

--- GameDesign/Assets/Scripts/HealthBox.cs
+++ GameDesign/Assets/Scripts/HealthBox.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class HealthBox : MonoBehaviour
 {
     public float health =20;
+    [SerializeField] private GameObject box;
+    [SerializeField] private Rigidbody2D rb;
     private Animator anim;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -21,11 +23,10 @@ public class HealthBox : MonoBehaviour
         if (health <= 0)
         {
             anim.SetTrigger("hit2");
-            Debug.Log("Enemy is dead");
-
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            box.GetComponent<BoxCollider2D>().enabled = false;
         }
     }
-
     public void DestroyBox()
     {
         Destroy(gameObject);
