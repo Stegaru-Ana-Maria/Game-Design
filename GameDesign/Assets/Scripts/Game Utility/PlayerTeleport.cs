b09da0125d39b18076e56bb07534f53890aa746f
Stegaru Ana Maria <stegaruanamaria94@yahoo.com>
2025-04-22 18:22:51 +0300

    I made a portal that can teleport the player to another zone on the map and I improved level visuals

---
+++ GameDesign/Assets/Scripts/Game Utility/PlayerTeleport.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            Transform destination = collision.GetComponent<Teleporter>().GetDestination();
+            if (destination != null)
+            {
+                transform.position = destination.position;
+            }
+        }
+    }
+}
